57ec8c164c7722a70e38dd258d50f815097b0e6b
hongweipeng <961365124@qq.com>
2016-09-12 14:56:02 +0800

    logo调转

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">
+                <a class="navbar-brand" href="<?php $this->options->siteUrl(); ?>">
                     <img alt="Brand" src="<?php $this->options->themeUrl('images/logo.png'); ?>"></a>
             </div>
             <div class="collapse navbar-collapse navbar-right " id="nav-1">
